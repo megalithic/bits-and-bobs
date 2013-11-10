e93205cf1f3c78dc3bbef928f094c80b6cc43706
Seth Messer <seth.messer@gmail.com>
2013-11-10 15:26:00 -0600

    added another npm

--- node/install.sh
+++ node/install.sh
@@ -2,10 +2,11 @@
 
 # installs yeoman, as well as grunt-cli and bower
 npm install -g yo #or lineman
+npm install -g protractor #used for e2e testing
 npm install
 
 # potential grunt addons (per project):
 # npm install grunt-notify --save-dev
 
 # potential grunt addons (global):
-# npm install -g lineman # alternative to yeoman?
+# npm install -g testem #used for unit testing (instead of karma with yo)
