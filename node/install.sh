d3a87f1d8db42a7990e3d7aa63ffb52d5170cb66
Seth Messer <seth.messer@gmail.com>
2013-11-10 15:12:36 -0600

    Add node and common npm installs

---
+++ node/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# installs yeoman, as well as grunt-cli and bower
+npm install -g yo #or lineman
+npm install
+
+# potential grunt addons (per project):
+# npm install grunt-notify --save-dev
+
+# potential grunt addons (global):
+# npm install -g lineman # alternative to yeoman?
