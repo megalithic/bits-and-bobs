67ff08332591c92d0b0a47c76fff6099b0971659
Seth Messer <seth.messer@gmail.com>
2013-11-12 10:33:42 -0600

    even more brew updates

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -26,7 +26,104 @@ echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH.
 brew install grc findutils spark zsh git
 brew install wget --enable-iri
 
-brew install ack apple-gcc42 autoconf automake avrdude bison chruby cmake coreutils cscope ctags faac ffind ffmpeg freetype fuse4x fuse4x-kext gawk gdbm gettext ghostscript gist git-extras git-flow glib gmp gnu-sed gnutls grep heroku-toolbelt htop-osx hub imagemagick jbig2dec jpeg jq jsl jslint lame libelf libevent libffi libftdi0 libgcrypt libgpg-error libksba libpng libsodium libtasn1 libtiff libtool libusb libusb-compat libxml2 libxslt libyaml lua mercurial mutt mysql nettle newt node openssl ossp-uuid p11-kit pcre phantomjs pkg-config popt python python3 qt readline reattach-to-user-namespace redis s-lang sbt sqlite ssh-copy-id sshfs ta-lib texi2html the_silver_searcher tmux todo-txt tokyo-cabinet tor vim watch wkhtmltopdf x264 xvid xz yasm zeromq zsh-completions zsh-lovers zsh-syntax-highlighting solr
+# TODO: need to get this cleaned up, lots of deps here
+brew install ack
+# apple-gcc42
+brew install autoconf
+brew install automake
+brew install avrdude
+brew install bison
+brew install chruby
+brew install cmake
+brew install cscope
+brew install ctags
+brew install faac
+brew install ffind
+brew install ffmpeg
+brew install freetype
+brew install gawk
+brew install gdbm
+brew install gettext
+brew install ghostscript
+brew install gist
+brew install git-extras
+brew install git-flow
+brew install glib
+brew install gmp
+brew install gnu-sed
+brew install gnutls
+brew install grep
+brew install heroku-toolbelt
+brew install htop-osx
+brew install hub
+brew install imagemagick
+brew install jbig2dec
+brew install jpeg
+brew install jq
+brew install jsl
+brew install jslint
+brew install lame
+brew install libelf
+brew install libevent
+brew install libffi
+brew install libftdi0
+brew install libgcrypt
+brew install libgpg-error
+brew install libksba
+brew install libpng
+brew install libsodium
+brew install libtasn1
+brew install libtiff
+brew install libtool
+brew install libusb
+brew install libusb-compat
+brew install libxml2
+brew install libxslt
+brew install libyaml
+brew install lua
+brew install mercurial
+brew install mutt
+brew install mysql
+brew install nettle
+brew install newt
+brew install node
+brew install openssl
+brew install ossp-uuid
+brew install p11-kit
+brew install pcre
+brew install phantomjs
+brew install pkg-config
+brew install popt
+brew install python
+brew install python3
+brew install qt
+brew install readline
+brew install reattach-to-user-namespace
+brew install redis
+brew install s-lang
+brew install sbt
+brew install sqlite
+brew install ssh-copy-id
+brew install sshfs
+brew install ta-lib
+brew install texi2html
+brew install the_silver_searcher
+brew install tmux
+brew install todo-txt
+brew install tokyo-cabinet
+brew install tor
+brew install vim
+brew install watch
+brew install wkhtmltopdf
+brew install x264
+brew install xvid
+brew install xz
+brew install yasm
+brew install zeromq
+brew install zsh-completions
+brew install zsh-lovers
+brew install zsh-syntax-highlighting
+brew install solr
 
 brew install weechat --with-ruby --with-python --with-perl
 #brew install macvim --with-cscope --with-lua --HEAD
