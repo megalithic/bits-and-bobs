8cac7253adbba6a87689324d622261fd9b386527
Seth Messer <seth.messer@gmail.com>
2013-11-20 10:10:32 -0600

    Fix some broken vimrc stuff

--- vim/install.sh
+++ vim/install.sh
@@ -5,3 +5,5 @@ git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -c ':BundleInstall' -c ':qa!'
 
 #cd ~/.vim/bundle/tern_for_vim && npm install
+
+ln -sf ~/.dotfiles/vim/vimrc.keybindings.vim ~/.vim/vimrc.keybindings.vim
