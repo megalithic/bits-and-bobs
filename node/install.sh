0a5eb2fc1c1a948a86b31981af4de0cf5181a368
Seth Messer <seth.messer@gmail.com>
2014-02-23 20:43:30 -0600

    Updates to teamocil config, vimrc, and npm default install

--- node/install.sh
+++ node/install.sh
@@ -3,6 +3,8 @@
 # installs yeoman, as well as grunt-cli and bower
 npm install -g yo #or lineman
 npm install -g karma
+npm install -g bower
+npm install -g jshint
 # npm install -g protractor #used for e2e testing
 npm install
 
