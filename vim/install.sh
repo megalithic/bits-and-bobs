3599879255b5aa7aab6aa495bb9997f7432b8dc1
Seth Messer <seth.messer@gmail.com>
2014-03-01 21:02:18 -0600

    Add custom vivify colors

--- vim/install.sh
+++ vim/install.sh
@@ -4,4 +4,5 @@ rm -rf ~/.vim/bundle/vundle
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -c ':BundleInstall' -c ':qa!'
 
-#cd ~/.vim/bundle/tern_for_vim && npm install
+cp -rf ~/.dotfiles/vim/colors ~/.vim/bundle/
+
