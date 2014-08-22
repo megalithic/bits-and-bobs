75203b7fc4f3b9ba72833a9312568ee61b1029bf
Seth Messer <seth.messer@gmail.com>
2014-08-21 21:17:58 -0500

    MINOR chanves to reenable vundle for setup

--- vim/install.sh
+++ vim/install.sh
@@ -3,15 +3,9 @@
 # ===============
 # SETUP vundle
 # ===============
-# rm -rf ~/.vim/bundle/vundle
-# git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-# vim -c ':BundleInstall' -c ':qa!'
-
-# ===============
-# SETUP vim-plug
-# ===============
-mkdir ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
+rm -rf ~/.vim/bundle/vundle
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim +PluginInstall +qall
 
 cp -rf ~/.dotfiles/vim/colors ~/.vim/bundle/
 ln -s ~/.dotfiles/vim/vimrc.symlink ~/.nvimrc
