1b4f120e39f358a65ddb75f01ee1adf9c83999ca
Seth Messer <seth.messer@gmail.com>
2014-10-25 16:30:15 -0500

    Update homebrew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,7 +9,7 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew doctor
@@ -24,17 +24,17 @@ brew install homebrew/dupes/grep
 brew install coreutils
 echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
 
-brew install grc findutils spark zsh git
+brew install grc findutils spark zsh git gh
 brew install wget --enable-iri
 
 brew tap homebrew/versions
-brew install gcc48
+# brew install gcc48
 brew tap Goles/battery
 
 
 # TODO: need to get this cleaned up, lots of deps here
 brew install ack
-brew install apple-gcc42
+# brew install apple-gcc42
 brew install autoconf
 brew install automake
 brew install avrdude
@@ -64,7 +64,6 @@ brew install gnutls
 brew install grep
 brew install heroku-toolbelt
 brew install htop-osx
-brew install hub
 brew install imagemagick
 brew install jbig2dec
 brew install jpeg
@@ -164,19 +163,17 @@ initdb /usr/local/var/postgres -E utf8
 ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
 brew link openssl --force
 brew link python --force
-sudo ln -s /usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/gcc-4.2 /usr/bin/gcc
-sudo ln -s /usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/g++-4.2 /usr/bin/g++
-sudo ln -s /usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/cpp-4.2 /usr/bin/cpp
 
-notmuch setup
-notmuch new
+# sudo ln -s /usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/gcc-4.2 /usr/bin/gcc
+# sudo ln -s /usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/g++-4.2 /usr/bin/g++
+# sudo ln -s /usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/cpp-4.2 /usr/bin/cpp
 
 # Install homebrew-cask so we can easily install GUI apps
-brew tap phinze/homebrew-cask
-brew install brew-cask
+# brew tap phinze/homebrew-cask
+# brew install brew-cask
 
 # Install quicklook plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package
+# brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package
 
 # Install homebrew-cask GUI apps
 # brew cask install alfred
@@ -213,8 +210,9 @@ brew linkapps
 brew cleanup
 
 # JDK
-curl -O http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-macosx-x64.dmg
-open jdk-7u45-macosx-x64.dmg
+
+curl -O http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-macosx-x64.dmg
+open jdk-7u71-macosx-x64.dmg
 # ==========================================================================
 
 exit 0
