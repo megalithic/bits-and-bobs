0cb648931ae4952ff289b828f3092ce29b2bdba5
Seth Messer <seth.messer@gmail.com>
2014-10-26 15:22:11 -0500

    Update install.sh

--- ruby/install.sh
+++ ruby/install.sh
@@ -4,8 +4,11 @@ echo "Installing chruby ..."
   source /usr/local/share/chruby/chruby.sh
   source /usr/local/share/chruby/auto.sh
 
-  ruby-install ruby 2.1
-  chruby ruby-2.1
+  ruby-install ruby 2.1.3
+  chruby ruby-2.1.3
+  
+echo "Permit user to write to system rubies and gems ..."
+  sudo chown -R replicant /Library/Ruby/Gems/2.0.0
 
 echo "Installing critical Ruby gems for Rails development ..."
   gem install bundler rails pg foreman thin pry launchy teamocil sass compass curses --no-rdoc --no-ri
