4f36eadb1165e39c7b9c0e13fc9829c2ffaf991d
Seth Messer <seth.messer@gmail.com>
2014-10-27 22:37:07 -0500

    minor changes to install script for postsql stuffs

--- system/env.zsh
+++ system/env.zsh
@@ -44,6 +44,8 @@ export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_GC_HEAP_FREE_SLOTS=500000
 export RUBY_GC_HEAP_INIT_SLOTS=40000
 
+export DATABASE_URL=postgres:///$(whoami)
+
 export ECLIPSE_HOME=/Applications/Eclipse
 export SSL_CERT_FILE=/usr/local/etc/openssl/cert.pem
 
