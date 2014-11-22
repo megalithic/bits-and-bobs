e9c0abcf0e9fb0732f3155dc19cddce1dd2d6563
Seth Messer <seth.messer@gmail.com>
2014-11-22 10:30:58 -0600

    changes to vimrc closely following a format devised by @knewter and I

--- node/install.sh
+++ node/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # installs yeoman, as well as grunt-cli and bower
-npm install -g grunt
+npm install -g grunt-cli
 npm install -g gulp
 npm install -g yo #or lineman
 npm install -g karma
+npm install -g karma-cli
 npm install -g bower
 npm install -g jshint
 npm install -g jsxhint
