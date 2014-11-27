c697daba0875614d49e5a9865919addb7a71f593
Seth Messer <seth.messer@gmail.com>
2014-11-27 17:07:18 -0600

    disable vundle usage for now

--- vim/install.sh
+++ vim/install.sh
@@ -3,17 +3,17 @@
 # ===============
 # SETUP vundle
 # ===============
-rm -rf ~/.vim/bundle/vundle
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +PluginInstall +qall
+# rm -rf ~/.vim/bundle/vundle
+# git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+# vim +PluginInstall +qall
 
 # ===============
 # SETUP vim-plug
 # ===============
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 
-cp -rf ~/.dotfiles/vim/colors ~/.vim/bundle/
 ln -sfv ~/.dotfiles/vim/vimrc.symlink ~/.nvimrc
 ln -sfv ~/.vim ~/.nvim
 ln -sfv ~/.dotfiles/vim/UltiSnips ~/.vim/
