53348b45ccfb2eb7afec7f9d4f81c5aad6bddf2d
Seth Messer <seth.messer@gmail.com>
2014-11-27 10:21:56 -0600

    Update to use vim-plug instead.

--- vim/install.sh
+++ vim/install.sh
@@ -7,6 +7,12 @@ rm -rf ~/.vim/bundle/vundle
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +PluginInstall +qall
 
+# ===============
+# SETUP vim-plug
+# ===============
+mkdir -p ~/.vim/autoload
+curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cp -rf ~/.dotfiles/vim/colors ~/.vim/bundle/
 ln -sfv ~/.dotfiles/vim/vimrc.symlink ~/.nvimrc
 ln -sfv ~/.vim ~/.nvim
