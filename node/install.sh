ca1f4871063a7e5195886c0923de947fc310bee1
Seth Messer <seth.messer@gmail.com>
2015-01-30 06:57:46 -0600

    Add ember to the global npm installs and tweak tmux conf a bit

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # installs yeoman, as well as grunt-cli and bower
-npm install -g grunt-cli gulp yo karma grunt karma-cli bower jshint jsxhint
+npm install -g grunt-cli gulp yo karma grunt karma-cli bower jshint jsxhint ember-cli
