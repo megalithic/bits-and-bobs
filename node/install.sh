203751cc3f7375ded0dfb7659944aeed3e9d5542
Seth Messer <seth.messer@gmail.com>
2015-02-10 21:14:13 -0600

    Updates mostly for node/nvm management

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# install nvm (0.23.3 as of this script)
+curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | zsh
+nvm install 0.12.0
+nvm install 0.10.36
+nvm install iojs
+nvm use 0.10.36 #currently most stable for me
+# nvm alias default iojs # make node default to iojs
+
 # installs yeoman, as well as grunt-cli and bower
-npm install -g grunt-cli gulp yo karma grunt karma-cli bower jshint jsxhint ember-cli traceur webpack 6to5
+npm install -g grunt-cli gulp yo karma grunt karma-cli bower jshint jsxhint ember-cli traceur webpack 6to5 reapp
