8e4bb02be3ef3a62c4e551d86520405fce298394
Seth Messer <seth.messer@gmail.com>
2015-03-11 20:17:51 -0500

    Necessary changes to go and vim for go keybindings

--- go/install.sh
+++ go/install.sh
@@ -10,9 +10,11 @@ mkdir $HOME/.go
 go get golang.org/x/tools/cmd/vet
 go get golang.org/x/tools/cmd/godoc
 go get github.com/tools/godep
-go get code.google.com/p/go.tools/cmd/goimports
+go get golang.org/x/tools/imports
 go get github.com/smartystreets/goconvey
 go get golang.org/x/tools/cmd/cover
+go get github.com/stretchr/testify/assert
+
 
 # get tags for go
 go get -u github.com/jstemmer/gotags
