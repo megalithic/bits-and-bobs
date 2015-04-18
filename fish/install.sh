31daeefc80251f0ab3a8cb3194452b9b236cabe9
Seth Messer <seth.messer@gmail.com>
2015-04-18 10:42:54 -0700

    Remove fish and fix some iterm setup stuffs

--- fish/install.sh
+++
@@ -1,5 +0,0 @@
-# Setup Fish as default shell and some other stuffs
-echo ' -- Setting login shell to fish '
-echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
-
--- iterm/install.sh
+++ iterm/install.sh
@@ -2,5 +2,5 @@
 
 #install iterm2 profile and preferences
 brew cask install iterm2
-tic xterm-256color-italic.terminfo
-tic screen-256color-italic.terminfo
+tic ~/.dotfiles/terminfo/xterm-256color-italic.terminfo
+tic ~/.dotfiles/terminfo/screen-256color-italic.terminfo
