3e3ca1da8dc6bbc32ac74c0fe4870ec11e11631d
Seth Messer <seth.messer@gmail.com>
2015-04-18 10:43:43 -0700

    Fix zsh setup files

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,8 +1,10 @@
-# Setup Fish as default shell and some other stuffs
-echo ' -- Setting login shell to fish '
-echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
+# Setup ZSH as default shell and some other stuffs
+echo ' -- Setting login shell to zsh '
+echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/zsh
 
 # to fix the issue described in the readme here:
 # https://github.com/skalnik/vim-vroom
 sudo cp ~/.dotfiles/zsh/zprofile.symlink /etc/zprofile
+
+source ~/.dotfiles/zsh/zshrc.symlink
