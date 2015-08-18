1612e37e6339d165c717112f5d330cb47978bf3d
Seth Messer <seth.messer@gmail.com>
2015-08-18 13:10:09 -0500

    Remove iterm stuffs for now

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #install iterm2 nightly
-curl -fLo ~/tmp/iterm2-nightly.zip https://iterm2.com/nightly/latest
-unzip ~/tmp/iterm2-nightly.zip
-mv ~/tmp/iTerm.app /Applications/
-rm ~/tmp/iterm2-nightly.zip
-
-tic ~/.dotfiles/terminfo/xterm-256color-italic.terminfo
-tic ~/.dotfiles/terminfo/screen-256color-italic.terminfo
+# curl -fLo ~/tmp/iterm2-nightly.zip https://iterm2.com/nightly/latest
+# unzip ~/tmp/iterm2-nightly.zip
+# mv ~/tmp/iTerm.app /Applications/
+# rm ~/tmp/iterm2-nightly.zip
+#
+# tic ~/.dotfiles/terminfo/xterm-256color-italic.terminfo
+# tic ~/.dotfiles/terminfo/screen-256color-italic.terminfo
