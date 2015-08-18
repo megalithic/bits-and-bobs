cf8191c36901e53a672b7071363c876cf8df9a0c
Seth Messer <seth.messer@gmail.com>
2015-08-18 07:57:16 -0500

    Updates to lib.sh and mysql.server sotp'

--- setup/lib.sh
+++ setup/lib.sh
@@ -16,27 +16,27 @@ COL_MAGENTA=$ESC_SEQ"35;01m"
 COL_CYAN=$ESC_SEQ"36;01m"
 
 function ok() {
-    echo -e "$COL_GREEN[ok]$COL_RESET "$1
+    echo "$COL_GREEN[ok]$COL_RESET "$1
 }
 
 function bot() {
-    echo -e "\n$COL_GREEN\[._.]/$COL_RESET - "$1
+    echo "\n$COL_GREEN\[._.]/$COL_RESET - "$1
 }
 
 function running() {
-    echo -en "$COL_YELLOW ⇒ $COL_RESET"$1": "
+    echo "$COL_YELLOW ⇒ $COL_RESET"$1": "
 }
 
 function action() {
-    echo -e "\n$COL_YELLOW[action]:$COL_RESET\n ⇒ $1..."
+    echo "\n$COL_YELLOW[action]:$COL_RESET\n ⇒ $1..."
 }
 
 function warn() {
-    echo -e "$COL_YELLOW[warning]$COL_RESET "$1
+    echo "$COL_YELLOW[warning]$COL_RESET "$1
 }
 
 function error() {
-    echo -e "$COL_RED[error]$COL_RESET "$1
+    echo "$COL_RED[error]$COL_RESET "$1
 }
 
 
