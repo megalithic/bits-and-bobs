68424ba3cca075fb6bd4ec76f2f7d3ea8fba3cc4
Seth Messer <seth.messer@gmail.com>
2015-09-03 16:16:12 -0500

    Eshint changes, fix git-pr binary

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -20,6 +20,24 @@ function fkill() {
   fi
 }
 
+wherearewe()
+{
+    if [[ -n "$SSH_CLIENT$SSH2_CLIENT$SSH_TTY" ]] ; then
+        echo ssh
+    else
+        # TODO check on *BSD
+        local sess_src="$(who am i | sed -n 's/.*(\(.*\))/\1/p')"
+        local sess_parent="$(ps -o comm= -p $PPID 2> /dev/null)"
+        if [[ -z "$sess_src" || "$sess_src" = ":"* ]] ; then
+            echo lcl  # Local
+        elif [[ "$sess_parent" = "su" || "$sess_parent" = "sudo" ]] ; then
+            echo su   # Remote su/sudo
+        else
+            echo tel  # Telnet
+        fi
+    fi
+}
+
 # GIT Helpers
 # ============================================================================
 # fstash - easier way to deal with stashes
