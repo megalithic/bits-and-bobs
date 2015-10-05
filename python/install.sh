2563e0205182c5b31be8fb298275d5f61671e2ea
Seth Messer <seth.messer@gmail.com>
2015-10-04 21:10:14 -0500

    Setup changes to teamocil.

--- python/install.sh
+++ python/install.sh
@@ -1,3 +1,3 @@
 # easy_install SpoofMAC
-pip install gntp pync selenium goobook pycp websocket-client
+pip install gntp pync selenium goobook pycp websocket-client saws
 pip install --upgrade neovim
