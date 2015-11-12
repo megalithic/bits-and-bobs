7908beb0e3c28b2352256f583c0f5dfd6d7f8814
Seth Messer <seth.messer@gmail.com>
2015-11-11 22:37:07 -0600

    add super awesome npm module for running node servers as processes

--- node/install.sh
+++ node/install.sh
@@ -50,6 +50,7 @@ require_npm karma-cli
 require_npm livedown
 require_npm mocha
 require_npm node-inspector
+require_npm pm2 # http://stackoverflow.com/a/20661027/213904
 require_npm prettyjson
 require_npm speed-test
 require_npm standard
