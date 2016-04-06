28f350cfc470b182a7977bc0e58ca0d16dc09572
Seth Messer <seth.messer@gmail.com>
2016-04-06 06:26:56 -1000

    further streamline hammerspoon setup

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -1,15 +1,32 @@
--- Reload hammerspoon configs
-hs.hotkey.bind({"cmd", "ctrl", "shift"}, "R", function()
-  hs.reload()
-end)
-hs.alert.show("Config Reloaded")
+-----------------------------------------------------------------------------------
+--/ initialize /--
+-----------------------------------------------------------------------------------
+local basePath = os.getenv("HOME") .. "/.hammerspoon/"
+local imagePath = basePath .. 'assets/';
 
--- Lock
-hs.hotkey.bind({"cmd", "ctrl", "shift"}, 'L', function()
-  -- open straight to the lock screen
-  -- os.execute("open '/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app'")
-  -- open straight to the screensaver
-  os.execute("open '/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine'")
-end)
 
+-- require 'battery'
+-- require 'windows'
+-- require 'random'
 -- require('aerosnap')
+
+
+-- reload configurations
+----------------------------------------------------------
+function reloadConfig()
+  hs.alert.show("Config Reloaded")
+  hs.reload()
+end
+
+hs.hotkey.bind({"cmd", "ctrl", "shift"}, "R",  reloadConfig)
+hs.pathwatcher.new(basePath, reloadConfig):start()
+
+-- lock laptop
+----------------------------------------------------------
+function lockComputer()
+  -- open straight to the lock screen
+  os.execute("open '/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app'")
+  -- open straight to the screensaver
+  -- os.execute("open '/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine'")
+end
+hs.hotkey.bind({"cmd", "ctrl", "shift"}, 'L', lockComputer)
