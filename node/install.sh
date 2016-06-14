1bfecfeb87b5f4a4fdf65925b7eef630e493caa0
Seth Messer <seth.messer@gmail.com>
2016-06-14 08:22:33 -0500

    Add babel-standard in and start replacing custom install functions

--- node/install.sh
+++ node/install.sh
@@ -14,40 +14,40 @@ n lts
 ###############################################################################
 bot "Setup AVN"
 ###############################################################################
-npm install -g avn avn-n
+npm i -g avn avn-n
 avn setup
 
 ###############################################################################
 bot "NPM Globals..."
 ###############################################################################
-
-require_npm babel
-require_npm babel-eslint@5.0.0
-require_npm bible
-require_npm bower
-require_npm browserify
-require_npm bunyan
-require_npm fkill-cli
-require_npm gulp
-require_npm hicat
-require_npm instant-markdown-d
-require_npm jscpd
-require_npm jsinspect
-require_npm jsonlint
-require_npm livedown
-require_npm mocha
-require_npm chai
-require_npm node-inspector
-require_npm pm2 # http://stackoverflow.com/a/20661027/213904
-require_npm prettyjson
-require_npm react-native-cli
-require_npm speed-test
-require_npm standard@5.3.1
-require_npm snazzy
-require_npm traceur
-require_npm trash # https://github.com/sindresorhus/trash
-require_npm webpack
-require_npm vimdebug
-require_npm vtop # https://github.com/MrRio/vtop
-require_npm yo
+npm i -g babel
+npm i -g babel-eslint@5.0.0
+npm i -g babel-standard
+npm i -g bible
+npm i -g bower
+npm i -g browserify
+npm i -g bunyan
+npm i -g fkill-cli
+npm i -g gulp
+npm i -g hicat
+npm i -g instant-markdown-d
+npm i -g jscpd
+npm i -g jsinspect
+npm i -g jsonlint
+npm i -g livedown
+npm i -g mocha
+npm i -g chai
+npm i -g node-inspector
+npm i -g pm2 # http://stackoverflow.com/a/20661027/213904
+npm i -g prettyjson
+npm i -g react-native-cli
+npm i -g speed-test
+npm i -g standard@5.3.1
+npm i -g snazzy
+npm i -g traceur
+npm i -g trash # https://github.com/sindresorhus/trash
+npm i -g webpack
+npm i -g vimdebug
+npm i -g vtop # https://github.com/MrRio/vtop
+npm i -g yo
 
