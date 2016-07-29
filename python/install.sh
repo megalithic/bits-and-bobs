75ace3819ff9451c26e2e2f50bbbf7aa76dd110e
Seth Messer <seth.messer@gmail.com>
2016-07-29 08:00:44 -0500

    fix some python pip issues

--- python/install.sh
+++ python/install.sh
@@ -5,9 +5,7 @@ source ./setup/lib.sh
 info "installing python packages"
 
 pip install --upgrade pip
-pip install --upgrade pip2
-pip install pip3 gntp pync selenium goobook pycp websocket-client saws pymux neovim pygments
-pip2 install pip3 gntp pync selenium goobook pycp websocket-client saws pymux neovim pygments
+pip install gntp pync selenium goobook pycp websocket-client saws pymux neovim pygments setuptools
 
 # pip3 install -U --user neovim
 pip2 install -U --user neovim
