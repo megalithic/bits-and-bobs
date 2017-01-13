d0a07e232d6c199658a793f3e799f3059d3a440d
Seth Messer <seth.messer@gmail.com>
2017-01-12 21:42:40 -0600

    add autosuggest to zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -88,4 +88,4 @@ export FZF_DEFAULT_OPTS='
 # export FZF_COMPLETION_TRIGGER=''
 export FZF_TMUX_HEIGHT='20%'
 
-export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
+# export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
