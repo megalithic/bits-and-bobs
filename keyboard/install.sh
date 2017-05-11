faf3ed0759062d6fbef81c968764e7bd6b538dd8
Seth Messer <seth.messer@gmail.com>
2017-05-11 17:25:03 -0500

    some key repeat things

--- keyboard/install.sh
+++ keyboard/install.sh
@@ -16,6 +16,13 @@ grep -sq forward-word ~/.inputrc || cat $PWD/inputrc >> ~/.inputrc
 # Disable Dock icon for Hammerspoon
 defaults write org.hammerspoon.Hammerspoon MJShowDockIconKey -bool FALSE
 
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
 # Open Apps
 open /Applications/Hammerspoon.app
 open /Applications/Karabiner-Elements.app
