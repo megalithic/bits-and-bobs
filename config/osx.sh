5a0790e8303c6f1f91885883f2965d7edb537b9a
Seth Messer <seth.messer@gmail.com>
2017-05-13 15:03:07 -0500

    Update osx.sh

--- config/osx.sh
+++ config/osx.sh
@@ -6,12 +6,12 @@ ln -sfv ~/.dotfiles/config ~/.config
 ln -sfv ~/.config/nvim ~/.vim
 ln -sfv ~/.config/nvim/init.vim ~/.vimrc
 
-curl -fLo ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+# curl -fLo ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qall
 echo "finished installing vim-plug"
 
-mkdir -p ~/.config/nvim/after/indent
-wget https://github.com/pangloss/vim-javascript/blob/master/indent/javascript.vim -O ~/.config/nvim/after/indent/javascript.vim
+# mkdir -p ~/.config/nvim/after/indent
+# wget https://github.com/pangloss/vim-javascript/blob/master/indent/javascript.vim -O ~/.config/nvim/after/indent/javascript.vim
 
 ln -sfv ~/.config/nvim ~/.vim
 ln -sfv ~/.config/nvim/init.vim ~/.vimrc
