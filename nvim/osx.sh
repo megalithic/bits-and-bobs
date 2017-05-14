6f1202c29c6f26c6b2cc2ee4aa20882a38b343ee
Seth Messer <seth.messer@gmail.com>
2017-05-14 07:42:35 -0500

    small update to a thing

--- nvim/osx.sh
+++ nvim/osx.sh
@@ -4,7 +4,7 @@ echo "(neo)vim setup"
 
 mkdir -p ~/.config
 ln -sfv ~/.dotfiles/nvim ~/.config/nvim
-ln -sfv ~/.dotfiles/nvim ~/.vim
+# ln -sfv ~/.dotfiles/nvim ~/.vim
 ln -sfv ~/.dotfiles/nvim/init.vim ~/.vimrc
 
 # curl -fLo ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
