148ea462fdbd463f882429728809030123ae723f
Seth Messer <seth.messer@gmail.com>
2017-05-14 07:39:23 -0500

    Updates some things

--- nvim/osx.sh
+++ nvim/osx.sh
@@ -2,9 +2,10 @@
 
 echo "(neo)vim setup"
 
-ln -sfv ~/.dotfiles/config ~/.config
-ln -sfv ~/.config/nvim ~/.vim
-ln -sfv ~/.config/nvim/init.vim ~/.vimrc
+mkdir -p ~/.config
+ln -sfv ~/.dotfiles/nvim ~/.config/nvim
+ln -sfv ~/.dotfiles/nvim ~/.vim
+ln -sfv ~/.dotfiles/nvim/init.vim ~/.vimrc
 
 # curl -fLo ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
@@ -12,8 +13,5 @@ echo "finished installing vim-plug"
 
 # mkdir -p ~/.config/nvim/after/indent
 # wget https://github.com/pangloss/vim-javascript/blob/master/indent/javascript.vim -O ~/.config/nvim/after/indent/javascript.vim
-
-ln -sfv ~/.config/nvim ~/.vim
-ln -sfv ~/.config/nvim/init.vim ~/.vimrc
 
 echo "finished neovim setup"
