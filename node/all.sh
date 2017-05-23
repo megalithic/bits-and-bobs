16cc79fea250056f3dc7b87b1e0d76e1d949e673
Seth Messer <seth.messer@gmail.com>
2017-05-23 11:43:02 -0500

    fix some avn/n stuff

--- node/all.sh
+++ node/all.sh
@@ -2,13 +2,14 @@
 
 echo "does node things"
 
-curl -L https://git.io/n-install | bash -s -- -y latest lts
-
-export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
-
-[[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
+curl -L https://git.io/n-install | bash -s -- -y lts
+n lts
 
 npm i -g yarn
 
 cat ~/.dotfiles/node/packages | xargs yarn global add
+
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+[[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
+
 
