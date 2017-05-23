8f59785afaf35f92acb3cc0fa5c36abd0ac0d1b8
Seth Messer <seth.messer@gmail.com>
2017-05-23 11:48:32 -0500

    more avn setup fixes

--- node/all.sh
+++ node/all.sh
@@ -5,11 +5,11 @@ echo "does node things"
 curl -L https://git.io/n-install | bash -s -- -y lts
 n lts
 
-npm i -g yarn
-
-cat ~/.dotfiles/node/packages | xargs yarn global add
+cat ~/.dotfiles/node/packages | xargs npm i -g
 
 export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
 [[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
+
+avn setup
 
 
