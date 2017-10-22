91d53b8739916b24602e1e0704b3cde5d2408c7f
Seth Messer <seth.messer@gmail.com>
2017-10-21 20:55:23 -0500

    adds fisherman for fish plugin management

--- fish/osx.sh
+++ fish/osx.sh
@@ -5,4 +5,7 @@ echo "fish setup"
 mkdir -p ~/.config
 ln -sfv ~/.dotfiles/fish ~/.config/fish
 
+# install fisherman (fish plugin manager)
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+
 echo "finished fish setup"
