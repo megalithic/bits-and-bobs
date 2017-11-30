b7f5f127fda9842f04527d78f3a0725969bd0269
Seth Messer <seth.messer@gmail.com>
2017-11-30 16:03:10 -0600

    more python

--- python/osx.sh
+++ python/osx.sh
@@ -3,7 +3,7 @@
 echo "installing python packages"
 
 pip3 install --upgrade pip3
-pip3 install gntp pync selenium goobook pycp websocket-client saws pymux neovim pygments setuptools glances
+pip3 install gntp pync goobook pycp websocket-client saws neovim pygments setuptools glances
 pip3 install -U --user neovim
 
 echo "finished python setup"
