2dacc99c0b00edb0e24d5598d264facc8bea57f3
Seth Messer <seth.messer@gmail.com>
2017-11-30 16:02:13 -0600

    hopefully fix some pip things

--- python/osx.sh
+++ python/osx.sh
@@ -3,15 +3,7 @@
 echo "installing python packages"
 
 pip3 install --upgrade pip3
-pip3 install --upgrade pip2
-pip3 install --upgrade pip
-
 pip3 install gntp pync selenium goobook pycp websocket-client saws pymux neovim pygments setuptools glances
-
-# pip2 install --user neovim
-# pip install --user neovim
 pip3 install -U --user neovim
-pip2 install -U --user neovim
-# pip install -U --user neovim
 
 echo "finished python setup"
