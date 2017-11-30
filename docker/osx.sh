2bf2a23e03fb23b1ba3dbc0b62204c1ef125a473
Seth Messer <seth.messer@gmail.com>
2017-11-30 15:54:46 -0600

    fixes some things that were failing setup

--- docker/osx.sh
+++ docker/osx.sh
@@ -4,8 +4,10 @@ echo "sets up docker things"
 
 # Docker
 # https://docs.docker.com/machine/get-started/
-docker-machine ls
-docker-machine create --driver virtualbox default
-docker-machine ls
-eval "$(docker-machine env default)"
+
+# docker-machine ls
+# docker-machine create --driver virtualbox default
+# docker-machine ls
+# eval "$(docker-machine env default)"
+
 # docker ps
--- osx/osx.sh
+++ osx/osx.sh
@@ -1,30 +1,49 @@
+#!/usr/bin/env bash
+
 # ------------------
 # great reference:
 # https://github.com/herrbischoff/awesome-osx-command-line
+# https://mths.be/macos
 # ------------------
 
 COMPUTER_NAME := 'replibook'
+COMPUTER_NAME = 'replibook'
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# get password up front
+sudo -v
+
+
+# Keep-alive: update existing `sudo` time stamp until `osx/osx.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Set standby delay to 24 hours (default is 1 hour)
+# sudo pmset -a standbydelay 86400
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
 
 # Allow apps downloaded from "Anywhere"
 sudo spctl --master-disable
+
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # See secrets.blacktree.com
-chsh -s /usr/local/bin/zsh $(USER)
-sudo scutil --set ComputerName $(COMPUTER_NAME)
-sudo scutil --set HostName $(COMPUTER_NAME)
-sudo scutil --set LocalHostName $(COMPUTER_NAME)
+chsh -s /usr/local/bin/zsh $USER
+
+sudo scutil --set ComputerName $COMPUTER_NAME
+sudo scutil --set HostName $COMPUTER_NAME
+sudo scutil --set LocalHostName $COMPUTER_NAME
 sudo defaults write \
   /Library/Preferences/SystemConfiguration/com.apple.smb.server \
-  NetBIOSName -string $(COMPUTER_NAME)
+  NetBIOSName -string $COMPUTER_NAME
 
 # Disabled shadow in screenshots
-@defaults write com.apple.screencapture disable-shadow -bool true
-
-# Enalble Ctrl+Alt+cmd+t for darkmode
-@sudo defaults write /Library/Preferences/.GlobalPreferences.plist \
-  _HIEnableThemeSwitchHotKey -bool true
+defaults write com.apple.screencapture disable-shadow -bool true
 
 # no .DS_Store on network
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
@@ -36,10 +55,18 @@ defaults write com.apple.finder NewWindowTargetPath -string "file://$(HOME)/Down
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
 # Trackpad: enable tap to click for this user and for the login screen (1 enabled, 0 disabled)
-defaults write com.apple.AppleMultitouchTrackpad Clicking -int 0
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 0
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 0
+# defaults write com.apple.AppleMultitouchTrackpad Clicking -int 0
+# defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 0
+# defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 0
+
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
 # PRE-SIERRA:
 # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 # defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
@@ -72,10 +99,21 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool true
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
-# Disable auto-correction
-defaults write NSGlobalDomain WebAutomaticSpellingCorrectionEnabled -bool false
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain WebAutomaticSpellingCorrectionEnabled -bool false
 
 # scrollbars on always!
 defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
@@ -85,6 +123,19 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Disable Resume system-wide
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
 # remove all default icons on the dock (for when first setting up)
 defaults delete com.apple.dock persistent-apps
