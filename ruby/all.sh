b79c3a28aff5adc512aae676795ee5c8a40a7aff
Seth Messer <seth.messer@gmail.com>
2018-03-27 15:44:33 -0500

    Update all.sh

--- ruby/all.sh
+++ ruby/all.sh
@@ -2,13 +2,13 @@
 
 echo "setting up ruby things"
 
-ruby-install ruby 2.1.4
+ruby-install ruby 2.3.1
 
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
 RUBIES=(~/.rubies/*)
 
-chruby ruby-2.1.4
+chruby ruby-2.3.1
 
 cat ~/.dotfiles/ruby/default-gems.symlink | xargs gem install
 
