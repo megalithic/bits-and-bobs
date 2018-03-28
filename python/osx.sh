15abf4146028b261bad888c50bc2e1ea855428ce
Seth Messer <seth.messer@gmail.com>
2018-03-27 21:13:59 -0500

    Update osx.sh

--- python/osx.sh
+++ python/osx.sh
@@ -2,11 +2,7 @@
 
 echo "installing python packages"
 
-pip install --upgrade pip pip3
-pip install gntp pync goobook pycp websocket-client saws neovim pygments setuptools glances
-pip install -U --user neovim
-
-pip3 install --upgrade pip3
+pip3 install --upgrade pip
 pip3 install gntp pync goobook pycp websocket-client saws neovim pygments setuptools glances
 pip3 install -U --user neovim
 
