a7e5ed618b3b579cfaf613643701754ce6dd59dc
Seth Messer <seth.messer@gmail.com>
2018-03-27 21:26:49 -0500

    Update all.sh

--- node/all.sh
+++ node/all.sh
@@ -4,13 +4,13 @@ echo "does node things"
 
 # curl -L https://git.io/n-install | bash -s -- -y lts
 # source .
-n lts
+# n lts
 
 cat ~/.dotfiles/node/packages | xargs npm i -g
 
-export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
-[[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
+#export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+#[[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
 
-avn setup
+#avn setup
 
 
