ef118b8179d96e588c6da6f156c9c701eb3d3124
Seth Messer <seth.messer@gmail.com>
2018-03-27 20:54:18 -0500

    Delete osx.sh

--- kitty/osx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "kitty setup"
-
-mkdir -p ~/.config
-ln -sfv ~/.dotfiles/kitty ~/.config/kitty
-ln -sfv ~/.dotfiles/kitty/kitty.conf ~/Library/Preferences/kitty/kitty.conf
-
-echo "finished kitty setup"
