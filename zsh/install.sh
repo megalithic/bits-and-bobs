da97e889633a9662798a56622e62c9ec564f7069
Seth Messer <seth.messer@gmail.com>
2018-05-23 21:52:31 -0500

    new installer things; new zsh things

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,17 +1,29 @@
-#!/bin/sh
+#!/bin/zsh
 
-echo "extra zsh setup"
+# First install Prezto
+if [[ ! -d $HOMEDIR/.zprezto ]]
+then
+  echo "Installing Prezto (https://github.com/zsh-users/prezto)..."
+  git clone --recursive git://github.com/sorin-ionescu/prezto.git "$HOMEDIR/.zprezto"
+else
+  cd $HOMEDIR/.zprezto
+  git reset --hard
+  git pull origin master -f
+  cd $HOMEDIR
+fi
 
-# install/setup zim
-# git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
-# setopt EXTENDED_GLOB
-# for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
-#   user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-#   if [[ ! -a ${ZDOTDIR:-${HOME}}/.zshrc ]]; then
-#     touch ${user_file}
-#     ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
-#   fi
-# done
+# echo "extra zsh setup"
+
+# # install/setup zim
+# # git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
+# # setopt EXTENDED_GLOB
+# # for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
+# #   user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+# #   if [[ ! -a ${ZDOTDIR:-${HOME}}/.zshrc ]]; then
+# #     touch ${user_file}
+# #     ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
+# #   fi
+# # done
 
 echo "changing your login shell to zsh"
 zsh_path=$(which zsh)
@@ -19,13 +31,13 @@ grep -Fxq "$zsh_path" /etc/shells || sudo bash -c "echo $zsh_path >> /etc/shells
 chsh -s "$zsh_path" $USER
 zsh
 
-touch $HOME/.z-history
+# touch $HOME/.z-history
 
-# to fix the issue described in the readme here:
-# https://github.com/skalnik/vim-vroom
-sudo ln -sfv $HOME/.dotfiles/zsh/zprofile.symlink /etc/zprofile
-sudo ln -sfv $HOME/.dotfiles/zsh/zshenv.symlink /etc/zshenv
+# # to fix the issue described in the readme here:
+# # https://github.com/skalnik/vim-vroom
+# sudo ln -sfv $HOME/.dotfiles/zsh/zprofile.symlink /etc/zprofile
+# sudo ln -sfv $HOME/.dotfiles/zsh/zshenv.symlink /etc/zshenv
 
-# source ${ZDOTDIR:-${HOME}}/.zlogin
+# # source ${ZDOTDIR:-${HOME}}/.zlogin
 
 echo "finished extra zsh setup"
